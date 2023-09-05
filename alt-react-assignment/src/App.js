3e06a77fa2a13ce8b42bf8a68f84192b4501d6b3
jaycee-cpu93 <jamescoding93@gmail.com>
2023-09-05 22:47:20 +0100

    first react assignment

---
+++ alt-react-assignment/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className='count'>
+        <Counter />
+    </div>
+  );
+}
+
+export default App;
